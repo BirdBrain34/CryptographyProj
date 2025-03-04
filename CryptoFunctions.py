ceb8d7a7f0ddc8d3ec60a125996c7be6fc3bec57
BirdBrain34 <lanceatendido34@gmail.com>
2025-03-04 16:01:02 +0800

    UPDATE: Made CryptoFunctions module

---
+++ CryptoFunctions.py
@@ -0,0 +1,15 @@
+def cipher1():
+    # Aldjon Cipher
+    print("Aldjon Cipher selected")
+
+def cipher2():
+    # Noel Cipher
+    print("Noel Cipher selected")
+
+def cipher3():
+    # Lance Cipher
+    print("Lance Cipher selected")
+
+def cipher4():
+    # Mystery Cipher (WIP)
+    print("Mystery Cipher (WIP)")
--- Kryptos.py
+++ Kryptos.py
@@ -1,22 +1,22 @@
+import CryptoFunctions as crypt
+
 def mainMenu():
     options = ["1. Aldjon Ciper", "2. Noel Cipher", "3. Vernam Cipher", "4. Mystery Cipher", "5. Exit"]
     for i in range (len(options)):
         print(options[i])
 
 mainMenu()
-choice = input("Enter your chosen number: ")
+choice = ''
 
-def cipher1():
-    #Aldjon
-    return cipher1
-def cipher2():
-    #Noel    
-    return cipher2
-
-def cipher3():
-    #Lance
-    return cipher3
-
-def cipher4():
-    #WIP
-    return cipher4
+while int(choice) != 5:
+    choice = input("Enter your chosen number: ")
+    if int(choice) not in range(1,6):
+        print("Your number is invalid! Please input from 1 to 5.")
+    elif int(choice) == 1:
+        crypt.cipher1()
+    elif int(choice) == 2:
+        crypt.cipher2()
+    elif int(choice) == 3:
+        crypt.cipher3()
+    elif int(choice) == 4:
+        crypt.cipher4()
