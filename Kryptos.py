d0c791d4c9386fad909cf1c460436a765b947e14
BirdBrain34 <lanceatendido34@gmail.com>
2025-03-04 15:31:54 +0800

    UPDATE: Kryptos BP is made

--- Kryptos.py
+++ Kryptos.py
@@ -1,1 +1,22 @@
-print("Hello World")
+def mainMenu():
+    options = ["1. Aldjon Ciper", "2. Noel Cipher", "3. Vernam Cipher", "4. Mystery Cipher", "5. Exit"]
+    for i in range (len(options)):
+        print(options[i])
+
+mainMenu()
+choice = input("Enter your chosen number: ")
+
+def cipher1():
+    #Aldjon
+    return cipher1
+def cipher2():
+    #Noel    
+    return cipher2
+
+def cipher3():
+    #Lance
+    return cipher3
+
+def cipher4():
+    #WIP
+    return cipher4
