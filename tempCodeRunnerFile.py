89d0daaf7a86196b335eab73b6e55317e5ffaedb
BirdBrain34 <lanceatendido34@gmail.com>
2025-03-04 15:32:24 +0800

    UPDATE: Deleted temp file

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def mainMenu():
-    options = ["1. Aldjon Ciper", "2. Noel Cipher", "3. Vernam Cipher", "4. Mystery Cipher", "5. Exit"]
-    for i in range (len(options)):
-        print(options[i])
-
-mainMenu()
-choice = input("Enter your chosen number: ")
